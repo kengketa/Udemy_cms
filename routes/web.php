093fbfdfb3d247f1ee2d78c469f95b1ca527dfba
kengketa_white <kengketa@gmail.com>
2019-08-27 21:10:09 +0700

    Section 9 Raw SQL Queries-End

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-    //  return "Hello world";
-});
+// Route::get('/', function () {
+//     return view('welcome');
+//     //  return "Hello world";
+// });
 
 // Route::get('/about', function () {
 //     //return view('welcome');
@@ -44,15 +44,45 @@ Route::get('/', function () {
 //     //return "test";
 // });
 
-
-
-
-
 //Route::get('post/{id}','PostsController@index');
 
 //Route::resource('posts','PostsController');
 
-Route::get('/contact','PostsController@contact');
-
-Route::get('post/{id}/{name}/{password}','PostsController@show_post');
+// Route::get('/contact','PostsController@contact');
+// Route::get('post/{id}/{name}/{password}','PostsController@show_post');
 //Route::get('post','PostsController@show_post');
+
+/* 
+|------------------------------------------------
+Application Database Raw SQL quries
+|------------------------------------------------
+*/
+
+// Route::get('/insert', function () {
+//     DB::insert('insert into posts (title,content) values (?, ?)', ['PHP with Laravel','Laravel is great']);
+    
+// });
+
+// Route::get('/read', function () {
+
+//    $results = DB::select('select * from posts where id = ?', [1]);
+
+// //    foreach ($results as $post) {
+// //       return $post->title;
+// //    }
+
+// //return $results;
+// return var_dump($results);
+// });
+
+// Route::get('update', function () {
+//     $updated = DB::update('update posts set title = "Updated title" where id = ?', [1]);
+//     return $updated;
+// });
+
+Route::get('/delete', function () {
+
+   $deleted = DB::delete('delete from posts where id = ?', [2]);
+    return $deleted;
+    
+});
